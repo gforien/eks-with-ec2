1390336b36445ff6d3c4c08e4044a9f0881205b5
Gabriel Forien <gforien+dev@gmail.com>
2021-12-19 04:47:22 +0100

    feat: installation de kubernetes

--- main.tf
+++ main.tf
@@ -147,5 +147,41 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo newgrp docker
 docker run -p 80:80 -d hello-world
+
+# network config
+sudo modprobe br_netfilter
+lsmod | grep br_netfilter                               # test
+
+# network config 2
+cat <<EOOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOOF
+sudo sysctl --system | grep bridge-nf-call              # test
+
+# check container runtime
+ls -l /var/run/docker.sock                              # test
+
+# prepare kubeadm, kubelet and kubectl installation
+cat <<EOOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOOF
+
+# deactivate selinux
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# install kubeadm, kubelet and kubectl
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+kubeadm version && kubelet --version && kubectl version # test
+
 EOF
 }
