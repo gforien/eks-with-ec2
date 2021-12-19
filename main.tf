80f1facc07fb74d6bf122c0be026645881e4cd2e
Gabriel Forien <gforien+dev@gmail.com>
2021-12-19 01:16:04 +0100

    feat: on ajoute la route-table et le security-group

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 #-----------------------------------------------------------------------------------------
-# 1. VPC
+# 1. VPC, IG, route table, SG
 #-----------------------------------------------------------------------------------------
 resource "aws_vpc" "default" {
   cidr_block = "12.13.0.0/16"
@@ -22,7 +22,40 @@ resource "aws_vpc" "default" {
 resource "aws_internet_gateway" "default" {
   vpc_id = aws_vpc.default.id
 }
-
+resource "aws_route_table" "default" {
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+}
+resource "aws_security_group" "default" {
+  vpc_id = aws_vpc.default.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 443
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 22
+    protocol    = "ssh"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+  }
+}
 
 
 #-----------------------------------------------------------------------------------------
