bdcef8fbad317826f3dc4d87031d8dbc56060dee
Gabriel Forien <gforien+dev@gmail.com>
2021-12-19 02:57:45 +0100

    fix: Les plages de ports ouverts commençaient à 0

--- main.tf
+++ main.tf
@@ -34,19 +34,19 @@ resource "aws_security_group" "default" {
   vpc_id = aws_vpc.default.id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
   }
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
   }
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
   }
